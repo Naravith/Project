b669259bf8e006f3b3d76c409859c573172c0e25
unknown <drachen.za456@gmail.com>
2020-10-02 22:51:17 +0700

    Added RYU's self learning ver 1.0.0

---
+++ RYU/self_learning.py
@@ -0,0 +1,135 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, DEAD_DISPATCHER
+from ryu.controller.handler import MAIN_DISPATCHER, HANDSHAKE_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.ofproto import ether
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import arp
+from ryu.lib.packet import ipv4
+from ryu import utils
+
+class SelfLearningBYLuxuss(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SelfLearningBYLuxuss, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def _switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+        self.logger.info("Switch: %s  Connected", dpid)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        dpid = datapath.id
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        arp_pkt = pkt.get_protocol(arp.arp)
+        ip_pkt = pkt.get_protocol(ipv4.ipv4)
+
+        if isinstance(arp_pkt, arp.arp):
+            self.logger.info("ARP processing")
+            if self.mac_learning(dpid, eth.src, in_port):
+                self.arp_forwarding(msg, arp_pkt.src_ip, arp_pkt.dst_ip, eth)
+
+        if isinstance(ip_pkt, ipv4.ipv4):
+            self.logger.info("IPv4 Processing")
+            mac_to_port_table = self.mac_to_port.get(dpid)
+            if mac_to_port_table:
+                # know destination
+                if eth.dst in mac_to_port_table:
+                    out_port = mac_to_port_table[eth.dst]
+                    actions = [parser.OFPActionOutput(out_port)]
+                    match = parser.OFPMatch(in_port=in_port, eth_dst=eth.dst)
+                    self.add_flow(datapath, 1, match, actions)
+                    self.send_packet_out(datapath, msg.buffer_id, in_port,
+                                         out_port, msg.data)
+                    
+                # dont know destination
+                else:
+                    if self.mac_learning(dpid, eth.src, in_port):
+                        self.flood(msg)
+    
+    def _arp_forwarding(self, msg, src_ip, dst_ip, eth_pkt):
+        datapath = msg.datapath
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        out_port = self.mac_to_port[datapath.id].get(eth_pkt.dst)
+        if out_port is not None:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=eth_pkt.dst,
+                                    eth_type=eth_pkt.ethertype)
+            actions = [parser.OFPActionOutput(out_port)]
+            self.add_flow(datapath, 1, match, actions)
+            self.send_packet_out(datapath, msg.buffer_id, in_port,
+                                 out_port, msg.data)
+        else:
+            self.flood(msg)
+
+    def _mac_learning(self, dpid, src_mac, in_port):
+        self.mac_to_port.setdefault(dpid, {})
+        # check mac address
+        if src_mac in self.mac_to_port[dpid]:
+            if in_port != self.mac_to_port[dpid][src_mac]:
+                return False
+        else:
+            self.mac_to_port[dpid][src_mac] = in_port
+            return True
+
+    def _flood(self, msg):
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        out = self._build_packet_out(datapath, ofproto.OFP_NO_BUFFER,
+                                     ofproto.OFPP_CONTROLLER,
+                                     ofproto.OFPP_FLOOD, msg.data)
+        datapath.send_msg(out)
+        self.logger.info("Flooding msg")
+
+    def _build_packet_out(self, datapath, buffer_id, src_port, dst_port, data):
+        actions = []
+        if dst_port:
+            actions.append(datapath.ofproto_parser.OFPActionOutput(dst_port))
+
+        msg_data = None
+        if buffer_id == datapath.ofproto.OFP_NO_BUFFER:
+            if data is None:
+                return None
+            msg_data = data
+
+        out = datapath.ofproto_parser.OFPPacketOut(
+            datapath=datapath, buffer_id=buffer_id,
+            data=msg_data, in_port=src_port, actions=actions)
+        return out
+
+    def _send_packet_out(self, datapath, buffer_id, src_port, dst_port, data):
+        out = self._build_packet_out(datapath, buffer_id,
+                                     src_port, dst_port, data)
+        if out:
+            datapath.send_msg(out)
+
+    def _add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst)
+        datapath.send_msg(mod)
