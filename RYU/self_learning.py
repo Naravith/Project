fa6329cee8aa375ed1dce357787132ddea266733
root <drachen.za456@gmail.com>
2020-10-13 19:04:18 +0000

    Added RYU's Self Learning ver 1.1.28

--- RYU/self_learning.py
+++ RYU/self_learning.py
@@ -99,7 +99,10 @@ class SelfLearningBYLuxuss(app_manager.RyuApp):
         if self.check_first_dfs:
             sum_link1, sum_link2 = 0, 0
             for dp in self.datapath_for_del:
-                print("Port : {0} ||| {1}".format(dp.port, datapath.ofproto.OFPP_ANY))
+                print(dp.ports.keys())
+                for i in dp.ports:
+                    print(i == datapath.ofproto.OFPP_CONTROLLER, end='--')
+                print()
                 sum_link1 += len(dp.ports) - 2 
             for i in self.adjacency:
                 sum_link2 += len(self.adjacency[i])
