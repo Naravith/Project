59e3d438d4fdfcc2ccc57de3fa45cdb500c16a28
unknown <drachen.za456@gmail.com>
2020-10-15 12:26:07 +0700

    Added RYU's Self Learning ver 1.2.7
    Reset Mac-Address Table & Flow Table

--- RYU/self_learning.py
+++ RYU/self_learning.py
@@ -69,16 +69,15 @@ class SelfLearningBYLuxuss(app_manager.RyuApp):
 
     @set_ev_cls(event.EventHostAdd, MAIN_DISPATCHER)
     def host_add_handler(self, ev):
-        '''
-        print(type(ev))
-        print(ev)
+        #print(type(ev))
+        #print(ev)
         print(ev.host)
-        print(ev.host.ipv4)
-        print(ev.host.mac)
-        print(ev.host.port)
-        print(ev.host.port.dpid)
-        print(ev.host.port.port_no)
-        '''
+        #print(ev.host.ipv4)
+        #print(ev.host.mac)
+        #print(ev.host.port)
+        #print(ev.host.port.dpid)
+        #print(ev.host.port.port_no)
+        
         self.host_faucet[ev.host.port.dpid].append(ev.host.port.port_no)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -133,10 +132,6 @@ class SelfLearningBYLuxuss(app_manager.RyuApp):
             #print("Re-Routing")
             #self._re_routing(self.link_for_DL[random.randint(0, len(self.link_for_DL) - 1)])
             self.time_start = time.time()
-            print("Mac to Port :\n{0}".format(self.mac_to_port))
-            print('-' * 50)
-            print("Hosts :\n{0}".format(self.hosts))
-            print('-' * 50)
             for dp in self.datapath_for_del:
                 for out in self.adjacency[dp.id]:
                     self._del_flow(dp, self.adjacency[dp.id][out])
