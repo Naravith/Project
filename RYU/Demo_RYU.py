bb8de61ede48bdf12105eda5ae5c30f9e1aba59c
unknown <drachen.za456@gmail.com>
2021-02-17 21:34:42 +0700

    Demo ver 1.1.4

--- RYU/Demo_RYU.py
+++ RYU/Demo_RYU.py
@@ -164,7 +164,7 @@ class SelfLearningBYLuxuss(app_manager.RyuApp):
 
                     if throughput != -1:
                         print("Host {0}\nThroughput : {1} Mbits / sec\nX-axis : {2} Pkt / sec".format(i, throughput, pktpersec))
-                        filename = "Host_{0}".format(i)
+                        filename = "Host_{0}.csv".format(i)
                         if not os.path.isfile(filename):
                             self._append_list_as_row(filename, ['Throughput', 'Pkt/sec'])
                         self._append_list_as_row(filename, [throughput, pktpersec])
