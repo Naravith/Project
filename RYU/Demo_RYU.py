b7e40ad33c1c106cfe46f289ba492e684ad9d62e
unknown <drachen.za456@gmail.com>
2021-03-03 15:49:38 +0700

    Demo ver 1.1.16

--- RYU/Demo_RYU.py
+++ RYU/Demo_RYU.py
@@ -114,7 +114,7 @@ class SelfLearningBYLuxuss(app_manager.RyuApp):
                     if datapath.id == link[0]:
                         self._PortStatReq(datapath, self.adjacency[link[0]][link[1]])
             
-            if (time.time() - self.queue_for_re_routing[1]) > 30.0:
+            if (time.time() - self.queue_for_re_routing[1]) > 20.0:
                 if self.queue_for_re_routing[0] != []:
                     self._re_routing(self.queue_for_re_routing[0])
                     self.queue_for_re_routing[0], self.queue_for_re_routing[1] = [], time.time()
