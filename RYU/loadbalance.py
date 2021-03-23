2461322049743728830b1bc38a088253a3d3d839
unknown <drachen.za456@gmail.com>
2021-03-23 20:49:03 +0700

    LB ver 1.0.2

--- RYU/loadbalance.py
+++ RYU/loadbalance.py
@@ -5,17 +5,13 @@ from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.mac import haddr_to_bin
-from ryu.lib.packet import in_proto
 from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
 from ryu.lib.packet import arp
+from ryu.lib.packet import ethernet
 from ryu.lib.packet import ipv4
 from ryu.lib.packet import ipv6
-from ryu.lib.packet import icmp
 from ryu.lib.packet import ether_types
-from ryu.lib import mac
-from ryu.lib import hub
-from ryu.lib import ip
+from ryu.lib import mac, ip
 from ryu.topology.api import get_switch, get_link
 from ryu.app.wsgi import ControllerBase
 from ryu.topology import event
@@ -27,12 +23,12 @@ import os
 import random
 import time
 
-# Cisco Reference bandwidth = 1 Gbps
-REFERENCE_BW = 10000000
+# Cisco Reference bandwidth = 10 Mbps
+REFERENCE_BW = 100000
 
-DEFAULT_BW = 10000000
+DEFAULT_BW = 100000
 
-MAX_PATHS = float('Inf')
+MAX_PATHS = 2
 
 class ProjectController(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -50,241 +46,24 @@ class ProjectController(app_manager.RyuApp):
         self.adjacency = defaultdict(dict)
         self.bandwidths = defaultdict(lambda: defaultdict(lambda: DEFAULT_BW))
 
-        # Fake addresses only known to the controller
-        self.controller_ip = "10.0.0.100"
-        self.controller_mac = "dd:dd:dd:dd:dd:df"
-        self.ping_mac = "de:dd:dd:dd:de:dd"
-        self.ping_ip = "10.0.0.99"
-        self.delay = defaultdict(lambda: defaultdict(lambda: 0))
-
-        self.replied = []
-
-    def monitor_link(self, s1, s2):
+    def get_paths(self, src, dst):
         '''
-            Monitors link latency between two switches.
-            Sends ping packet every 0.5 second.
+        Get all paths from src to dst using DFS algorithm    
         '''
-        while True:
-            self.send_ping_packet(s1, s2)
-
-            hub.sleep(0.5)
-
-        self.logger.info('Stop monitoring link %s %s' % (s1.dpid, s2.dpid))
-
-    def monitor_link_controller(self, s1):
-        '''
-            Monitors link latency between two switches.
-            Sends ping packet every 0.5 second.
-        '''
-        while True:
-            self.send_ping_packet_controller(s1)
-
-            hub.sleep(0.5)
-
-
-    def send_ping_packet_controller(self, s1):
-        '''
-            Send a ping/ICMP packet between two switches.
-            Uses ryu's packet library.
-            Uses a fake MAC and IP address only known to controller.
-        '''
-        datapath = s1
-        dst_mac = self.ping_mac
-        dst_ip = self.ping_ip
-        out_port = datapath.ofproto.OFPP_CONTROLLER
-        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
-
-        pkt = packet.Packet()
-        pkt.add_protocol(ethernet.ethernet(ethertype=ether_types.ETH_TYPE_IP,
-                                           src=self.controller_mac,
-                                           dst=dst_mac))
-        pkt.add_protocol(ipv4.ipv4(proto=in_proto.IPPROTO_ICMP,
-                                   src=self.controller_ip,
-                                   dst=dst_ip))
-        echo_payload = '%s;%s;%f' % (s1.id, 0, time.time())
-        payload = icmp.echo(data=echo_payload)
-        pkt.add_protocol(icmp.icmp(data=payload))
-        pkt.serialize()
-
-        out = datapath.ofproto_parser.OFPPacketOut(
-            datapath=datapath,
-            buffer_id=datapath.ofproto.OFP_NO_BUFFER,
-            data=pkt.data,
-            in_port=datapath.ofproto.OFPP_CONTROLLER,
-            actions=actions
-        )
-
-        datapath.send_msg(out)
-
-    def send_ping_packet(self, s1, s2):
-        '''
-            Send a ping/ICMP packet between two switches.
-            Uses ryu's packet library.
-            Uses a fake MAC and IP address only known to controller.
-        '''
-        datapath = self.datapath_list[int(s1.dpid)]
-        dst_mac = self.ping_mac
-        dst_ip = self.ping_ip
-        out_port = s1.port_no
-        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
-
-        pkt = packet.Packet()
-        pkt.add_protocol(ethernet.ethernet(ethertype=ether_types.ETH_TYPE_IP,
-                                           src=self.controller_mac,
-                                           dst=dst_mac))
-        pkt.add_protocol(ipv4.ipv4(proto=in_proto.IPPROTO_ICMP,
-                                   src=self.controller_ip,
-                                   dst=dst_ip))
-        echo_payload = '%s;%s;%f' % (s1.dpid, s2.dpid, time.time())
-        payload = icmp.echo(data=echo_payload)
-        pkt.add_protocol(icmp.icmp(data=payload))
-        pkt.serialize()
-
-        out = datapath.ofproto_parser.OFPPacketOut(
-            datapath=datapath,
-            buffer_id=datapath.ofproto.OFP_NO_BUFFER,
-            data=pkt.data,
-            in_port=datapath.ofproto.OFPP_CONTROLLER,
-            actions=actions
-        )
-
-        datapath.send_msg(out)
-
-    def ping_packet_handler(self, pkt):
-        '''
-            Handler function when ping packet arrives.
-            Extracts the data from the packet and calculates the latency.
-        '''
-        icmp_packet = pkt.get_protocol(icmp.icmp)
-        echo_payload = icmp_packet.data
-        payload = echo_payload.data
-        info = payload.split(';')
-        s1 = info[0]
-        s2 = info[1]
-        latency = (time.time() - float(info[2])) * 1000  # in ms
-        # print "s%s to s%s latency = %f ms" % (s1, s2, latency)
-        self.delay[int(s1)][int(s2)] = latency
-    
-    def minimum_distance(self, distance, Q):
-        min = float('Inf')
-        node = 0
-        for v in Q:
-            if distance[v] < min:
-                min = distance[v]
-                node = v
-        return node
-
-    def path(self, previous, node_start, node_end):
-        r=[]
-        p=node_end
-        r.append(p)
-        q=previous[p]
-        while q is not None:
-            if q == node_start:
-                r.append(q)
-                break
-            p=q
-            r.append(p)
-            q=previous[p]
-    
-        r.reverse()
-        if node_start==node_end:
-            path=[node_start]
-        else:
-            path=r
-        return r
-    
-    def dijkstra(self,graph, node_start, node_end=None):
-        distances = {}     
-        previous = {}  
-    
-        distances = defaultdict(lambda: float('Inf'))
-        previous = defaultdict(lambda: None)
-    
-        distances[node_start]=0
-        Q=set(switches)
-    
-        while len(Q)>0:
-            u = minimum_distance(distances, Q)
-            Q.remove(u)  
-    
-            for p in switches:
-                if graph[u][p]!=None:
-                    w = delay[u][p] 
-                    if distances[u] + w < distances[p]:
-                        distances[p] = distances[u] + w
-                        previous[p] = u
-    
-        if node_end:
-            return {'cost': distances[node_end],
-                    'path': path(previous, node_start, node_end)}
-        else:
-            return (distances, previous)
-    
-    def get_paths(self, src,dst,first_port,final_port,max_k=2):
-        print("YenKSP is called")
-        print("src=",src," dst=",dst, " first_port=", first_port, " final_port=", final_port, " max_k=", max_k) 
-        adjacency2=defaultdict(lambda:defaultdict(lambda:None))
-        
-        distances, previous = dijkstra(self.adjacency,src)
-        A = [{'cost': distances[dst],
-                'path': path(previous, src, dst)}]
-        B = []
-        #print "distances=", distances
-        #print  "previous=", previous
-        #print "A=", A
-        
-        if not A[0]['path']: return A
-        
-        try:
-            for k in range(1, max_k):
-                adjacency2=copy.deepcopy(self.adjacency)
-                #print "k=", k, " adjacency2=", adjacency2
-                for i in range(0, len(A[-1]['path']) - 1):
-                    node_spur = A[-1]['path'][i]
-                    path_root = A[-1]['path'][:i+1]
-                #print "node_spur=", node_spur, " path_root=", path_root
-            
-                    for path_k in A:
-                        curr_path = path_k['path']
-                #print "curr_path=", curr_path, " i=", i
-                        if len(curr_path) > i and path_root == curr_path[:i+1]:
-                            adjacency2[curr_path[i]][curr_path[i+1]]=None
-                #print "link[", curr_path[i],"][", curr_path[i+1], "] is removed"
-                
-                    path_spur = dijkstra(adjacency2, node_spur, dst)
-            #print "path_spur=", path_spur
-        
-                    if path_spur['path']:
-                        path_total = path_root[:-1] + path_spur['path']
-                #print "path_total=", path_total
-                        dist_total = distances[node_spur] + path_spur['cost']
-                #print "dist_total=", path_total
-                        potential_k = {'cost': dist_total, 'path': path_total}
-                #print "potential_k=", potential_k
-        
-                        if not (potential_k in B):
-                            B.append(potential_k)
-                #print "B=", B
-        
-                if len(B):
-                    B = sorted(B, key=itemgetter('cost'))
-                #print "after sorting, B=", B
-                    A.append(B[0])
-                    B.pop(0)
-            #print "after poping out the first element, B=", B, " A=", A
+        if src == dst:
+            # host target is on the same switch
+            return [[src]]
+        paths = []
+        stack = [(src, [src])]
+        while stack:
+            (node, path) = stack.pop()
+            for next in set(self.adjacency[node].keys()) - set(path):
+                if next is dst:
+                    paths.append(path + [next])
                 else:
-                    break
-        except:
-            pass
-    
-        tmp=[]
-        print("YenKSP->")
-        for path_k in A:
-            print(path_k)
-            tmp.append(path_k)
-    
-        return [x['path'] for x in A]
+                    stack.append((next, path + [next]))
+        print("Available paths from ", src, " to ", dst, " : ", paths)
+        return paths
 
     def get_link_cost(self, s1, s2):
         '''
@@ -342,12 +121,12 @@ class ProjectController(app_manager.RyuApp):
 
     def install_paths(self, src, first_port, dst, last_port, ip_src, ip_dst):
         computation_start = time.time()
-        paths = self.get_paths(src, dst)
+        paths = self.get_optimal_paths(src, dst)
         pw = []
         for path in paths:
             pw.append(self.get_path_cost(path))
             print(path, "cost = ", pw[len(pw) - 1])
-        sum_of_pw = sum(pw)
+        sum_of_pw = sum(pw) * 1.0
         paths_with_ports = self.add_ports_to_paths(paths, first_port, last_port)
         switches_in_paths = set().union(*paths)
 
@@ -383,7 +162,7 @@ class ProjectController(app_manager.RyuApp):
                 )
 
                 out_ports = ports[in_port]
-                print(out_ports) 
+                # print out_ports 
 
                 if len(out_ports) > 1:
                     group_id = None
@@ -494,11 +273,6 @@ class ProjectController(app_manager.RyuApp):
         src = eth.src
         dpid = datapath.id
 
-        if dst == self.ping_mac:
-            # ping packet arrives
-            self.ping_packet_handler(pkt)
-            return
-
         if src not in self.hosts:
             self.hosts[src] = (dpid, in_port)
 
@@ -523,7 +297,7 @@ class ProjectController(app_manager.RyuApp):
                     out_port = self.install_paths(h1[0], h1[1], h2[0], h2[1], src_ip, dst_ip)
                     self.install_paths(h2[0], h2[1], h1[0], h1[1], dst_ip, src_ip) # reverse
 
-        print(pkt)
+        # print pkt
 
         actions = [parser.OFPActionOutput(out_port)]
 
@@ -537,8 +311,8 @@ class ProjectController(app_manager.RyuApp):
         datapath.send_msg(out)
 
     @set_ev_cls(event.EventSwitchEnter)
-    def switch_enter_handler(self, event):
-        switch = event.switch.dp
+    def switch_enter_handler(self, ev):
+        switch = ev.switch.dp
         ofp_parser = switch.ofproto_parser
 
         if switch.id not in self.switches:
@@ -548,25 +322,30 @@ class ProjectController(app_manager.RyuApp):
             # Request port/link descriptions, useful for obtaining bandwidth
             req = ofp_parser.OFPPortDescStatsRequest(switch)
             switch.send_msg(req)
-            hub.spawn(self.monitor_link_controller, switch)
 
     @set_ev_cls(event.EventSwitchLeave, MAIN_DISPATCHER)
-    def switch_leave_handler(self, event):
-        print(event)
-        switch = event.switch.dp.id
+    def switch_leave_handler(self, ev):
+        print(ev)
+        switch = ev.switch.dp.id
         if switch in self.switches:
-            del self.switches[switch]
+            self.switches.remove(switch)
             del self.datapath_list[switch]
             del self.adjacency[switch]
 
     @set_ev_cls(event.EventLinkAdd, MAIN_DISPATCHER)
-    def link_add_handler(self, event):
-        s1 = event.link.src
-        s2 = event.link.dst
+    def link_add_handler(self, ev):
+        s1 = ev.link.src
+        s2 = ev.link.dst
         self.adjacency[s1.dpid][s2.dpid] = s1.port_no
         self.adjacency[s2.dpid][s1.dpid] = s2.port_no
-        hub.spawn(self.monitor_link, s1, s2)
 
     @set_ev_cls(event.EventLinkDelete, MAIN_DISPATCHER)
-    def link_delete_handler(self, event):
-        return
+    def link_delete_handler(self, ev):
+        s1 = ev.link.src
+        s2 = ev.link.dst
+        # Exception handling if switch already deleted
+        try:
+            del self.adjacency[s1.dpid][s2.dpid]
+            del self.adjacency[s2.dpid][s1.dpid]
+        except KeyError:
+            pass
