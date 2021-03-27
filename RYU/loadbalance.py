ebcf64a168a90a0e4d4f7a28bb5109d4347a645a
unknown <drachen.za456@gmail.com>
2021-03-27 18:04:41 +0700

    LB ver 1.0.5

--- RYU/loadbalance.py
+++ RYU/loadbalance.py
@@ -15,6 +15,7 @@ from ryu.lib import mac, ip
 from ryu.topology.api import get_switch, get_link
 from ryu.app.wsgi import ControllerBase
 from ryu.topology import event
+from ryu.lib import hub
 
 from collections import defaultdict
 from operator import itemgetter
@@ -22,6 +23,9 @@ from operator import itemgetter
 import os
 import random
 import time
+import csv
+import inspect
+from random import randint
 
 # Cisco Reference bandwidth = 10 Mbps
 REFERENCE_BW = 10000000
@@ -45,6 +49,195 @@ class ProjectController(app_manager.RyuApp):
         self.group_ids = []
         self.adjacency = defaultdict(dict)
         self.bandwidths = defaultdict(lambda: defaultdict(lambda: DEFAULT_BW))
+        self.datapath_for_del = []
+        self.check_first_dfs = 1
+        self.host_faucet = defaultdict(list)
+        self.Hosts_add = {}
+        self.topo = []
+        self.link_for_DL = []
+        self.csv_filename = {}
+        self.all_path = {}
+        self.flow_stat_links = defaultdict(list)
+        self.flow_timestamp = defaultdict(list)
+        self.port_stat_links = defaultdict(list)
+
+    def _TrafficMonitor(self):
+        while True:
+            for datapath in self.datapath_for_del:
+                if (time.time() - self.time_start) > 15:
+                    self._FlowStatReq(datapath)
+                for link in self.link_for_DL:
+                    if datapath.id == link[0]:
+                        self._PortStatReq(datapath, self.adjacency[link[0]][link[1]])
+            '''
+            if (time.time() - self.queue_for_re_routing[1]) > 20.0:
+                if self.queue_for_re_routing[0] != []:
+                    self._re_routing(self.queue_for_re_routing[0])
+                    self.queue_for_re_routing[0], self.queue_for_re_routing[1] = [], time.time()
+                    self.print_bw_util = []
+            else:
+                self.queue_for_re_routing[0] = []
+                self.print_bw_util = []
+            '''
+            
+            
+            hub.sleep(1)
+
+    def _PortStatReq(self, datapath, port_no):
+        parser = datapath.ofproto_parser
+
+        req = parser.OFPPortStatsRequest(datapath=datapath, flags=0, port_no=port_no)
+        datapath.send_msg(req)
+
+    def _FlowStatReq(self, datapath):
+        parser = datapath.ofproto_parser
+
+        req = parser.OFPFlowStatsRequest(datapath)
+        datapath.send_msg(req)
+
+    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    def _flow_stats_reply_handler(self, ev):
+        msg = ev.msg
+
+        flow_stat_reply = msg.to_jsondict()
+        sum_bytes = {}
+        sum_pkts = {}
+
+        for i in self.Hosts_add.keys():
+            sum_bytes[i] = 0
+            sum_pkts[i] = 0
+
+        for i in flow_stat_reply['OFPFlowStatsReply']['body']:
+            if i['OFPFlowStats']['match']['OFPMatch']['oxm_fields'] != []:
+                
+                out_port = i['OFPFlowStats']['instructions'][0]['OFPInstructionActions']['actions'][0]['OFPActionOutput']['port']
+                byte_count = i['OFPFlowStats']['byte_count']
+                pkt_count = i['OFPFlowStats']['packet_count']
+                in_port = -1
+                eth_dst = -1
+                eth_type = -1
+
+                for j in i['OFPFlowStats']['match']['OFPMatch']['oxm_fields']:
+                    if j['OXMTlv']['field'] == 'in_port':
+                        in_port = j['OXMTlv']['value']
+                    elif j['OXMTlv']['field'] == 'eth_dst': 
+                        eth_dst = j['OXMTlv']['value']
+                    elif j['OXMTlv']['field'] == 'eth_type':
+                        eth_type = j['OXMTlv']['value']
+                
+                if eth_type not in [2054, 35020]:
+                    for host_port in self.host_faucet[ev.msg.datapath.id]:
+                        if out_port == host_port:
+                            sum_bytes[eth_dst] += byte_count
+                            sum_pkts[eth_dst] += pkt_count
+        
+        for i in [k for k, v in self.Hosts_add.items() if v[0] == ev.msg.datapath.id]:
+            tmp = "HOST-{0}".format(i)
+            self.flow_stat_links[tmp].append([sum_bytes[i], sum_pkts[i], time.time()])
+            while len(self.flow_stat_links[tmp]) >= 3:
+                self.flow_stat_links[tmp].pop(0)
+            
+            if len(self.flow_stat_links[tmp]) == 2:
+                if (self.flow_stat_links[tmp][1][0] - self.flow_stat_links[tmp][0][0]) > 10000:
+                    if (i not in self.flow_timestamp) or (len(self.flow_timestamp[i]) == 0):
+                        self.flow_timestamp[i].append(self.flow_stat_links[tmp][0].copy())
+                else:
+                    throughput = -1
+                    if (i in self.flow_timestamp) and (len(self.flow_timestamp[i]) == 1):
+                        start_byte = self.flow_timestamp[i][0][0]
+                        start_pkt = self.flow_timestamp[i][0][1]
+                        start_time = self.flow_timestamp[i][0][2]
+                        cur_byte = self.flow_stat_links[tmp][0][0]
+                        cur_pkt = self.flow_stat_links[tmp][0][1]
+                        cur_time = self.flow_stat_links[tmp][0][2]
+                        self.flow_timestamp[i].pop(0)
+                        throughput = ((cur_byte - start_byte) / (cur_time - start_time)) * 8
+                        pktpersec = (cur_pkt - start_pkt) / (cur_time - start_time)
+
+                    if throughput != -1:
+                        filename = "Host_{0}.csv".format(i).replace(":", "-")
+                        if not os.path.isfile(filename):
+                            self._append_list_as_row(filename, ['Throughput', 'Pkt/sec'])
+                        self._append_list_as_row(filename, [throughput, pktpersec])
+
+    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    def _port_stats_reply_handler(self, ev):
+        msg = ev.msg
+        port_stat_reply = msg.to_jsondict()
+        port_stat = port_stat_reply['OFPPortStatsReply']['body'][0]['OFPPortStats']
+        tx_p, tx_b = port_stat['tx_packets'], port_stat['tx_bytes']
+        rx_p, rx_b = port_stat['rx_packets'], port_stat['rx_bytes']
+        tx_d, rx_d = port_stat['tx_dropped'], port_stat['rx_dropped']
+        tmp = "S{0}-P{1}".format(msg.datapath.id, port_stat['port_no'])
+        self.port_stat_links[tmp].append([tx_p, rx_p, tx_b, rx_b, tx_d, rx_d])
+
+        for dst_switch, values in self.adjacency[msg.datapath.id].items():
+            if values == port_stat['port_no']:
+                check_more_than_zero = True
+                filename = self.csv_filename["[{0}, {1}]".format(msg.datapath.id, dst_switch)]
+                if not os.path.isfile(filename):
+                    self._append_list_as_row(filename, ['Timestamp', 'Tx_Packet', 'Rx_Packet', 'Dropped', 'BW_Utilization'])
+                if len(self.port_stat_links[tmp]) == 1:
+                    bw_util = (self.port_stat_links[tmp][0][2] + self.port_stat_links[tmp][0][3]) / 1310720
+                    dropped = self.port_stat_links[tmp][0][4] + self.port_stat_links[tmp][0][5]
+                    row_contents = [time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()), self.port_stat_links[tmp][0][0], \
+                        self.port_stat_links[tmp][0][1], dropped, bw_util * 1310720]
+                    '''
+                    random_val = (randint(3, 9) / 100)
+                    if bw_util + random_val > 0.65 and ([msg.datapath.id, dst_switch] not in self.queue_for_re_routing[0]):
+                        self.queue_for_re_routing[0].append([msg.datapath.id, dst_switch])
+                        self.print_bw_util.append([msg.datapath.id, dst_switch, bw_util, bw_util + random_val])
+                    '''
+                    if bw_util < 1e-03:
+                        check_more_than_zero = False
+
+                elif len(self.port_stat_links[tmp]) == 2:
+                    bw_util = ((self.port_stat_links[tmp][1][2] - self.port_stat_links[tmp][0][2]) + \
+                                (self.port_stat_links[tmp][1][3] - self.port_stat_links[tmp][0][3])) / 1310720
+                    dropped = (self.port_stat_links[tmp][1][4] - self.port_stat_links[tmp][0][4]) + \
+                        (self.port_stat_links[tmp][1][5] - self.port_stat_links[tmp][0][5])
+                    row_contents = [time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()), self.port_stat_links[tmp][1][0] - self.port_stat_links[tmp][0][0], \
+                        self.port_stat_links[tmp][1][1] - self.port_stat_links[tmp][0][1], dropped, bw_util * 1310720]
+                    '''
+                    random_val = (randint(3, 9) / 100)
+                    if bw_util + random_val > 0.65 and ([msg.datapath.id, dst_switch] not in self.queue_for_re_routing[0]):
+                        self.queue_for_re_routing[0].append([msg.datapath.id, dst_switch])
+                        self.print_bw_util.append([msg.datapath.id, dst_switch, bw_util, bw_util + random_val])
+                    '''
+                    if bw_util < 1e-03:
+                        check_more_than_zero = False
+
+                if check_more_than_zero:
+                    self._append_list_as_row(filename, row_contents)
+                    '''
+                    number = int(filename.split('./link')[1].split('.csv')[0])
+                    if number not in self.data_for_train:
+                        self.data_for_train[number] = []
+                    self.data_for_train[number].append([row_contents[-1]])
+                    '''
+        
+        print("Switch : {0} || Port : {1}".format(msg.datapath.id, port_stat['port_no']))
+        if len(self.port_stat_links[tmp]) == 1:
+            print("Tx : {0} packets | Rx:{1} packets".format(self.port_stat_links[tmp][0][0], self.port_stat_links[tmp][0][1]))
+            print("BW Utilization (10 Mbps) : {0} %".format((self.port_stat_links[tmp][0][2] + \
+                self.port_stat_links[tmp][0][3]) / 1310720 * 100))
+        elif len(self.port_stat_links[tmp]) == 2:
+            print("Tx : {0} packets | Rx:{1} packets".format(self.port_stat_links[tmp][1][0] - self.port_stat_links[tmp][0][0]\
+                , self.port_stat_links[tmp][1][1]- self.port_stat_links[tmp][0][1]))
+            print("BW Utilization (10 Mbps) : {0} %".format(((self.port_stat_links[tmp][1][2] - self.port_stat_links[tmp][0][2]) + \
+                            (self.port_stat_links[tmp][1][3] - self.port_stat_links[tmp][0][3])) / 1310720 * 100))
+        print("+" * 50)
+        
+
+
+        if len(self.port_stat_links[tmp]) == 2:
+            self.port_stat_links[tmp].pop(0)
+        
+
+    def _append_list_as_row(self, file_name, list_of_elem):
+        with open(file_name, 'a+', newline='') as write_obj:
+            csv_writer = csv.writer(write_obj)
+            csv_writer.writerow(list_of_elem)
 
     def get_paths(self, src, dst):
         '''
@@ -240,6 +433,8 @@ class ProjectController(app_manager.RyuApp):
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
+        self.datapath_for_del.append(datapath)
+        print("Switch : {0} Connected".format(datapath.id))
 
     @set_ev_cls(ofp_event.EventOFPPortDescStatsReply, MAIN_DISPATCHER)
     def port_desc_stats_reply_handler(self, ev):
@@ -254,6 +449,20 @@ class ProjectController(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
+
+        if self.check_first_dfs:
+            sum_link1, sum_link2 = 0, 0
+            for dp in self.datapath_for_del:
+                for i in dp.ports:
+                    if i != 4294967294 and (i not in self.host_faucet[dp.id]):
+                        sum_link1 += 1
+            for i in self.adjacency:
+                sum_link2 += len(self.adjacency[i])
+            if sum_link1 == sum_link2 and sum_link1 and sum_link2:
+                for i in self.switches:
+                    self.topo.append(sorted(self.adjacency[i]))
+                self.check_first_dfs = 0
+                self._get_paths()
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocol(ethernet.ethernet)
@@ -316,7 +525,7 @@ class ProjectController(app_manager.RyuApp):
                     out_port = mac_to_port_table[dst]
                     actions = [parser.OFPActionOutput(out_port)]
                     match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-                    self._add_flow(datapath, 1, match, actions)
+                    self.add_flow(datapath, 1, match, actions)
                     self._send_packet_out(datapath, msg.buffer_id, in_port,
                                          out_port, msg.data)
                 else:
@@ -338,6 +547,38 @@ class ProjectController(app_manager.RyuApp):
         datapath.send_msg(out)
         '''
 
+    def _get_paths(self):
+        cnt = 1
+        for x in self.switches:
+            for y in self.switches:
+                if x != y:
+                    if y in self.adjacency[x].keys() and [x, y] not in self.link_for_DL and [x, y][::-1] not in self.link_for_DL:
+                        self.link_for_DL.append([x, y])
+                        self.csv_filename.setdefault(str([x, y]), {})
+                        self.csv_filename[str([x, y])] = "./link{0}.csv".format(cnt)
+                        cnt += 1
+                    key_link, mark, path = str(x) + '->' + str(y), [0] * len(self.switches), []
+                    self.all_path.setdefault(key_link, {})
+                    mark[x - 1] = 1
+                    self._dfs(x, y, [x], self.topo, mark, path)
+                    self.all_path[key_link] = sorted(path, key = len)
+        
+        print("Topology All Path :")
+        for i in self.all_path:
+            print(i, ":", self.all_path[i])
+
+    def _dfs(self, start, end, k, topo, mark, path):
+        if k[-1] == end:
+            if len(k) == len(set(k)):
+                path.append(k[:])
+        for i in range(len(topo[start - 1])):
+            if mark[topo[start - 1][i] - 1] == 0:
+                mark[topo[start - 1][i] - 1] = 1
+                k.append(topo[start - 1][i])
+                self._dfs(topo[start - 1][i], end, k, topo, mark, path)
+                k.pop()
+                mark[topo[start - 1][i] - 1] = 0
+
     @set_ev_cls(event.EventSwitchEnter)
     def switch_enter_handler(self, ev):
         switch = ev.switch.dp
@@ -345,6 +586,7 @@ class ProjectController(app_manager.RyuApp):
 
         if switch.id not in self.switches:
             self.switches.append(switch.id)
+            self.switches = sorted(self.switches)
             self.datapath_list[switch.id] = switch
 
             # Request port/link descriptions, useful for obtaining bandwidth
@@ -388,7 +630,7 @@ class ProjectController(app_manager.RyuApp):
             match = parser.OFPMatch(in_port=in_port, eth_dst=eth_pkt.dst,
                                     eth_type=eth_pkt.ethertype)
             actions = [parser.OFPActionOutput(out_port)]
-            self._add_flow(datapath, 1, match, actions)
+            self.add_flow(datapath, 1, match, actions)
             self._send_packet_out(datapath, msg.buffer_id, in_port,
                                  out_port, msg.data)
         else:
@@ -411,7 +653,6 @@ class ProjectController(app_manager.RyuApp):
                                      ofproto.OFPP_CONTROLLER,
                                      ofproto.OFPP_FLOOD, msg.data)
         datapath.send_msg(out)
-        #self.logger.info("Flooding msg")
 
     def _build_packet_out(self, datapath, buffer_id, src_port, dst_port, data):
         actions = []
@@ -432,17 +673,10 @@ class ProjectController(app_manager.RyuApp):
     def _send_packet_out(self, datapath, buffer_id, src_port, dst_port, data):
         out = self._build_packet_out(datapath, buffer_id,
                                      src_port, dst_port, data)
-        #print("dpid : {0} | src : {1} | dst : {2}\nout : {3}".format(datapath.id, src_port, dst_port, out))
         if out:
             datapath.send_msg(out)
 
-    def _add_flow(self, datapath, priority, match, actions):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
-
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                match=match, instructions=inst)
-        datapath.send_msg(mod)
+    @set_ev_cls(event.EventHostAdd, MAIN_DISPATCHER)
+    def host_add_handler(self, ev):
+        HOST = ev.host
+        self.host_faucet[HOST.port.dpid].append(HOST.port.port_no)
